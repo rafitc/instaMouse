34d58193b6e3eef3c7562094808878dcb5b3ce49
rafitc <rafirasheed.tc99@gmail.com>
2020-05-28 10:26:28 +0530

    added comments in code

--- new.py
+++ new.py
@@ -2,14 +2,14 @@ import pyautogui as pg
 import keyboard
 import time
 
-while True:
-	if(keyboard.is_pressed('h')):
+while True: #for infinite loop
+	if(keyboard.is_pressed('h')): #checking pressed key is "h"
 		print('Liked photo')
-		pg.click(clicks=2)
-		time.sleep(0.3)
-	if(keyboard.is_pressed('p')):
+		pg.click(clicks=2) #Two click neededd for like a post
+		time.sleep(0.3)	#To avoid multiple entry 
+	if(keyboard.is_pressed('p')): #checking pressed key is "p"
 		print('video played')
 		pg.click()
-		time.sleep(0.3)
+		time.sleep(0.3)	#To avoid multiple entry 
 
 
